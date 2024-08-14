5c4fd036cfae22edd43911e16ee0f386ea1b34ca
rajashekhar-a <rajuavunuri123@gmail.com>
2024-08-14 14:06:57 +0530

    Create the Jenkinsfile with shared library to build the docker image

---
+++ vars/docker.groovy
@@ -0,0 +1,41 @@
+def call(Map params = [:]) {
+
+    def args = [
+            COMPONENT                  : '',
+            LABEL                      : 'master'
+    ]
+    args << params
+
+    pipeline {
+        agent {
+            label params.LABEL
+        }
+
+        stages {
+            stage('Labeling Build') {
+                steps {
+                    script {
+                        str = GIT_BRANCH.split('/').last()
+                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "BRANCH = ${str}"
+                    }
+                }
+            }
+
+            stage('Docker Build') {
+                steps {
+                  sh """
+                  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 975050272810.dkr.ecr.us-east-1.amazonaws.com
+                  docker build -t 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:latest .
+                  docker push 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:latest
+                  """
+                }
+            }
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}

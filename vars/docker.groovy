9ea54ae46cdee1908c7c364b9dde4ad4011e51d9
rajashekhar-a <rajuavunuri123@gmail.com>
2024-08-14 15:43:29 +0530

    update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -22,10 +22,17 @@ def call(Map params = [:]) {
             }
 
             stage('Docker Build') {
+                when {
+                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                }
                 steps {
                   sh """
+                  GIT_TAG=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'
+                  echo \${GIT_TAG} >version
                   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 975050272810.dkr.ecr.us-east-1.amazonaws.com
-                  docker build -t 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:latest .
+                  docker build -t 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:\${GIT_TAG} .
+                  docker push 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:\${GIT_TAG}
+                  docker tag 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:\${GIT_TAG} 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:latest
                   docker push 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:latest
                   """
                 }

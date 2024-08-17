b16dd864efd00d3dc1b2f831d4260cf8a77da2e2
rajashekhar-a <rajuavunuri123@gmail.com>
2024-08-17 20:24:14 +0530

    update jenkinsfile

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -16,23 +16,18 @@ def call(Map params = [:]) {
                 steps {
                     script {
                         str = GIT_BRANCH.split('/').last()
+                        addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "COMPONENT = ${params.COMPONENT}"
                         addShortText background: 'yellow', color: 'black', borderColor: 'yellow', text: "BRANCH = ${str}"
                     }
                 }
             }
 
             stage('Docker Build') {
-                when {
-                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
-                }
                 steps {
                   sh """
-                  GIT_TAG=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'
-                  echo \${GIT_TAG} >version
                   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 975050272810.dkr.ecr.us-east-1.amazonaws.com
-                  docker build -t 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:\${GIT_TAG} .
-                  docker push 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:\${GIT_TAG}
-                  docker tag 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:\${GIT_TAG} 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:latest
+                  docker build -t 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT} .
+                  docker tag 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:
                   docker push 975050272810.dkr.ecr.us-east-1.amazonaws.com/${params.COMPONENT}:latest
                   """
                 }

2c4bf4d30b3db3cb25dd2698efa3ccba69190746
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-25 23:34:22 +0530

    update changes

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -32,7 +32,7 @@ def call(Map params = [:]) {
 
             stage('uplode artifacts') {
                 when {
-                    expression { sh([ returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags' ]) }
+                    expression { sh([ returnStdout: true, script: 'echo ${env.GIT_BRANCH} | grep tags || true' ]) }
                 }
                 script {
                    sh'echo upload artifacts'

482fc1b3b01aa8c1d9f76ca6610a3f699970cf21
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-26 22:42:25 +0530

    update sonar token

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -27,7 +27,7 @@ def call(Map params = [:]) {
             stage('Submit Code Quality') {
                 steps {
                     sh """
-                      sonar-scanner -Dsonar.projectKey=${params.COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.33.183:9000 -Dsonar.token=sqp_33ac861ce67fb94b8909bd66792c714b8caeb632
+                      sonar-scanner -Dsonar.projectKey=${params.COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.39.179:9000 -Dsonar.token=sqp_b67d70b6d68c9af9a9a23efcb4df943ad8be35e9
                       echo ok
                     """
                 }
@@ -36,7 +36,7 @@ def call(Map params = [:]) {
             stage('Check Code Quality Gate') {
                 steps {
                     sh """
-                      sonar-quality-gate.sh admin admin123 172.31.28.20 ${params.COMPONENT}
+                      sonar-quality-gate.sh admin admin123 172.31.39.179 ${params.COMPONENT}
                       """
                 }
             }

06e7f497278e74ad101b677c2ab3fdd3a11e8887
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-26 22:31:30 +0530

    check code quality gate

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -26,7 +26,18 @@ def call(Map params = [:]) {
 
             stage('Submit Code Quality') {
                 steps {
-                    sh "sonar-scanner -Dsonar.projectKey=${params.COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.33.183:9000 -Dsonar.token=sqp_33ac861ce67fb94b8909bd66792c714b8caeb632"
+                    sh """
+                      sonar-scanner -Dsonar.projectKey=${params.COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.33.183:9000 -Dsonar.token=sqp_33ac861ce67fb94b8909bd66792c714b8caeb632
+                      echo ok
+                    """
+                }
+            }
+
+            stage('Check Code Quality Gate') {
+                steps {
+                    sh """
+                      sonar-quality-gate.sh admin admin123 172.31.28.20 ${params.COMPONENT}
+                      """
                 }
             }
 

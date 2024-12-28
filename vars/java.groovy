3c4272499eb1b2f94f62ed775834a0ef9517a386
rajashekhar-a <rajuavunuri123@gmail.com>
2024-12-28 21:43:34 +0530

    skip code quality check

--- vars/java.groovy
+++ vars/java.groovy
@@ -33,7 +33,7 @@ def call(Map params = [:]) {
             stage('Submit Code Quality') {
                 steps {
                     sh """
-                      sonar-scanner -Dsonar.projectKey=${params.COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.39.179:9000 -Dsonar.token=sqp_b67d70b6d68c9af9a9a23efcb4df943ad8be35e9
+                      #sonar-scanner -Dsonar.projectKey=${params.COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.39.179:9000 -Dsonar.token=sqp_b67d70b6d68c9af9a9a23efcb4df943ad8be35e9
                       env
                     """
                 }
@@ -42,7 +42,7 @@ def call(Map params = [:]) {
             stage('Check Code Quality Gate') {
                 steps {
                     sh """
-                      sonar-quality-gate.sh admin admin123 172.31.39.179 ${params.COMPONENT}
+                      #sonar-quality-gate.sh admin admin123 172.31.39.179 ${params.COMPONENT}
                       echo ok
                       """
                 }
